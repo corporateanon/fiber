2927d4f449d89933f800a2330d4b7944ed4c568f
no <no>
2020-03-06 17:00:41 +0200

    add call counter

--- src/fiber.test.ts
+++ src/fiber.test.ts
@@ -1,42 +1,58 @@
-import axios from 'axios';
-import { fun, wait } from './fiber';
+import axios from "axios";
+import { fun, wait } from "./fiber";
 
-describe('Fiber', () => {
-    it('should run', async () => {
-        const get = fun(axios.get);
+describe("Fiber", () => {
+    it("should run", async () => {
+        const get = fun(axios.get, "axios.get");
 
         await wait(() => {
             const httpBinResponse = get(
-                `https://httpbin.org/get?q=${get('https://example.com').status}`
+                `https://httpbin.org/get?q=${get("https://example.com").status}`
             );
-            expect(httpBinResponse.data.args).toEqual({ q: '200' });
+            expect(httpBinResponse.data.args).toEqual({ q: "200" });
         });
     });
-    it('should fail', () => {
-        const get = fun(axios.get);
+
+    it("should fail", () => {
+        const get = fun(axios.get, "axios.get");
 
         expect(
             wait(() => {
-                const res = get('https://aaaa-never-existed.aaaa');
-                console.log(res);
+                get("https://aaaa-never-existed.aaaa");
             })
-        ).rejects.toMatchObject({ code: 'ENOTFOUND' });
+        ).rejects.toMatchObject({ code: "ENOTFOUND" });
     });
-    it.only('should log', async () => {
-        const get = fun(axios.get);
+
+    it("should re-evaluate idempotent function if it is called multiple times in wait", async () => {
+        const get = fun(axios.get, "axios.get");
+
+        const logSpy = jest.fn();
         const log = fun(data => {
-            console.log(data);
-        });
+            logSpy(data);
+        }, "console.log");
 
         await wait(() => {
-            log(get('https://example.com').status);
-
-            log(
-                get('https://example.com/non-existent', {
-                    validateStatus: s => s < 500
-                }).status
-            );
-            log(get('https://example.com').status);
+            log(get("https://example.com").status);
+            log(get("https://example.com").status);
+            log("foo");
+            log("foo");
         });
+
+        expect(logSpy.mock.calls).toMatchInlineSnapshot(`
+            Array [
+              Array [
+                200,
+              ],
+              Array [
+                200,
+              ],
+              Array [
+                "foo",
+              ],
+              Array [
+                "foo",
+              ],
+            ]
+        `);
     });
 });
--- src/fiber.ts
+++ src/fiber.ts
@@ -1,41 +1,65 @@
 class Cache<Args extends any[], Result> {
     private hashToValue: Map<string, Result> = new Map();
 
-    private argsToHash(args: Args) {
-        return JSON.stringify(args);
+    private argsToHash(args: Args, callNumber: number) {
+        return JSON.stringify([callNumber, ...args]);
     }
 
-    public has(args: Args): boolean {
-        return this.hashToValue.has(this.argsToHash(args));
+    public has(args: Args, callNumber: number): boolean {
+        return this.hashToValue.has(this.argsToHash(args, callNumber));
     }
 
-    public get(args: Args): Result | undefined {
-        const hash = this.argsToHash(args);
+    public get(args: Args, callNumber: number): Result | undefined {
+        const hash = this.argsToHash(args, callNumber);
         return this.hashToValue.get(hash);
     }
 
-    public set(args: Args, value: Result): void {
-        this.hashToValue.set(this.argsToHash(args), value);
+    public set(args: Args, value: Result, callNumber: number): void {
+        this.hashToValue.set(this.argsToHash(args, callNumber), value);
     }
 }
 
+function sleep() {
+    return new Promise(res => setImmediate(res));
+}
+
 export function fun<Result, Args extends any[]>(
-    f: (...args: Args) => Result | Promise<Result>
+    f: (...args: Args) => Result | Promise<Result>,
+    _logName: string
 ) {
-    const memo = new Cache<Args, Result>();
+    const evaluationCache = new Cache<Args, Result>();
+
+    let callNumberInCurrentStack = 0;
+    const resetCallNumber = () => {
+        callNumberInCurrentStack = 0;
+    };
+    let timer = setImmediate(resetCallNumber);
+
     const callableFunc = (...args: Args) => {
-        if (memo.has(args)) {
-            return memo.get(args) as Result;
+        clearImmediate(timer);
+        timer = setImmediate(resetCallNumber, 0);
+
+        callNumberInCurrentStack++;
+
+        if (evaluationCache.has(args, callNumberInCurrentStack)) {
+            return evaluationCache.get(
+                args,
+                callNumberInCurrentStack
+            ) as Result;
         }
+        const callNumberOfThisEvaluation = callNumberInCurrentStack;
         const pms = new Promise<Result>((res, rej) => {
             try {
+                // console.log(
+                //     `${callNumberInCurrentStack}. Evaluating ${logName}(${args})`
+                // );
                 Promise.resolve(f(...args)).then(res, rej);
             } catch (e) {
                 rej(e);
             }
         });
         throw pms.then(result => {
-            memo.set(args, result);
+            evaluationCache.set(args, result, callNumberOfThisEvaluation);
         });
     };
     return callableFunc;
@@ -44,7 +68,10 @@ export function fun<Result, Args extends any[]>(
 export async function wait(func: () => any) {
     let pms: Promise<any> | null = Promise.resolve();
 
+    // let i = 0;
     while (pms) {
+        // console.log(`Iteration: ${i}`);
+        // i++;
         await pms;
         try {
             func();
@@ -52,6 +79,7 @@ export async function wait(func: () => any) {
         } catch (e) {
             if (e instanceof Promise) {
                 pms = e;
+                await sleep();
             } else {
                 throw e;
             }
